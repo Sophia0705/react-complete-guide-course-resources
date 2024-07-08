4aa119115eb0b253e9ecf4b0124155131c1b32b7
Sophia Lee <lllyyj75@gmail.com>
2024-07-08 17:27:32 +0900

    ~42

--- attachments/03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
+++ attachments/03 React Essentials/01-starting-project/01-starting-project/src/App.jsx
@@ -1,10 +1,20 @@
+import reactImg from "./assets/react-core-concepts.png"
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)]
+
   // 괄호 써야 하는데 vscode에서 문서 정렬 단축키(Format Document) 누르면 automatically
   return (<header>
-    <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+    <img src={ reactImg } alt="Stylized atom" />
     <h1>React Essentials</h1>
     <p>
-      Fundamental React concepts you will need for almost any app you are
+      { description } React concepts you will need for almost any app you are
       going to build!
     </p>
   </header>
